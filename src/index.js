b8e75e46dcaf4ebc2e19f1f7fa80d77c356eae58
Neyko-qt <fauster08@mail.ru>
2018-03-01 00:23:47 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  var minBase = 2;
+
+  if (base >= minBase && base <= 256) {
+    
+    while (minBase < base) {
+
+      if (base % minBase == 0) {
+        base /= minBase;
+      }  
+      minBase++;
+    }
+  }
+
+  var zerosCount = 0;
+
+  while (number >= 1 && number <= 10e7) {
+    number = Math.floor(number / base);
+    zerosCount += number;
+  }
+
+    return zerosCount; 
 }
